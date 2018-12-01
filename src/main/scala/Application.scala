db55d9fbea2f06460f6c2d23f6f81956e0cf7d3e
Anton Semenov <tochka313@gmail.com>
2018-12-01 15:02:52 +0300

    Added substrings weight matrix

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -3,11 +3,9 @@ import ru.bmstu.bioinformatics.scoring.{SubstringWeightMatrix, WeightMatrix}
 object Application {
 
   def main(args: Array[String]): Unit = {
-    println(WeightMatrix.readDefault.keys.map(_._2).toList.sorted)
-    println(WeightMatrix.readDefault('H', 'H'))
 
-    val keys = WeightMatrix.readDefault.keys.map(_._1)
-    println(SubstringWeightMatrix.allStrings(keys, 1, keys.map(_.toString).toList))
-    println(SubstringWeightMatrix.allStrings(keys, 1, keys.map(_.toString).toList).size)
+    val sstrwm = SubstringWeightMatrix(WeightMatrix.readDefault)
+    println(sstrwm.size)
+    sstrwm.foreach(println)
   }
 }
--- src/main/scala/ru/bmstu/bioinformatics/scoring/SubstringWeightMatrix.scala
+++ src/main/scala/ru/bmstu/bioinformatics/scoring/SubstringWeightMatrix.scala
@@ -2,8 +2,6 @@ package ru.bmstu.bioinformatics.scoring
 
 import ru.bmstu.bioinformatics.scoring.WeightMatrix.KeyMatrix
 
-import scala.collection.mutable
-
 /** Represents symmetrical weight matrix, where keys are strings of a given size. */
 object SubstringWeightMatrix {
 
@@ -11,10 +9,11 @@ object SubstringWeightMatrix {
 
   /** In order to satisfy symmetry property for [[SubstringWeightMatrix]], [[KeyMatrix]] is assumed to be symmetrical as well*/
   def apply(m: KeyMatrix, size: Int = 2): SubstringWeightMatrix = {
-    ???
+    val chars = m.keys.map(_._1)
+    apply(m, allStrings(chars, size - 1, chars.map(_.toString)))
   }
 
-  def allStrings(chars: Iterable[Char], rem: Int, acc: List[String]): List[String] = {
+  def allStrings(chars: Iterable[Char], rem: Int, acc: Iterable[String]): Iterable[String] = {
     if (rem > 0) {
       val newAcc = for {
         s <- acc
@@ -26,11 +25,24 @@ object SubstringWeightMatrix {
     }
   }
 
-  private def apply(keys: Iterable[Char], curr: String, rem: Int, acc: mutable.HashMap[(String, String), Int]): Unit = {
-    if (rem > 0) {
-      keys.foreach { c =>
-        apply(keys, curr + c, rem - 1, acc)
-      }
+  private def apply(m: KeyMatrix, keys: Iterable[String]): SubstringWeightMatrix = {
+    println(keys.size)
+    val init = for {
+      s1 <- keys
+      s2 <- keys if s2 >= s1
+    } yield {
+      (s1, s2) -> computeScore(m, s1, s2)
+    }
+
+    val map = init.toMap
+    map.withDefault {
+      case (s1, s2) if s2 < s1 => map(s2, s1)
+    }
+  }
+
+  private def computeScore(m: KeyMatrix, s1: String, s2: String): Int = {
+    s1.zip(s2).foldLeft(0) {
+      case (acc, (c1, c2)) => acc + m((c1, c2))
     }
   }
 }
