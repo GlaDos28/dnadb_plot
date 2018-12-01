b94e2e0b0715535cd13ad14f75b05231a8a1cd05
Anton Semenov <tochka313@gmail.com>
2018-12-01 14:17:28 +0300

    Added reading weight matrix

---
+++ src/main/scala/ru/bmstu/bioinformatics/scoring/WeightMatrix.scala
@@ -0,0 +1,82 @@
+package ru.bmstu.bioinformatics.scoring
+
+import java.io.File
+
+import scala.collection.mutable
+import scala.io.Source
+
+object WeightMatrix {
+
+  type KeyMatrix = Map[(Char, Char), Int]
+
+  def default: KeyMatrix = fromResource("protein.mtx")
+
+  def fromResource(name: String): KeyMatrix = {
+    fromFile(getResourceFile(name))
+  }
+
+  /* Any lines starting with # are discarded
+    The first line (excluding comments) should contain only space separated latin characters or
+    an asterisk (any not-stated latin character)
+    The body of the (square) matrix should consist of lines starting with a latin character or
+    an asterisk which is followed by a series of numbers corresponding to the size of the matrix
+   */
+  def fromFile(fileInput: File): KeyMatrix = {
+    val builder = mutable.HashMap[(Char, Char), Int]()
+    val names :: body = Source.fromFile(fileInput)
+      .getLines()
+      .map(_.trim)
+      .filter(!_.startsWith("#"))
+      .toList
+
+    val nameArray = splitBySpaces(names).map { n =>
+      assert(n.length == 1)
+      n.head
+    }
+
+    assert(nameArray.length == body.length)
+    val excludedSet = ('A' to 'Z').toSet.diff(nameArray.toSet)
+
+    body.foreach { line =>
+      //First element of row is a letter
+      val lineArray = splitBySpaces(line)
+      assert(lineArray(0).length == 1)
+      val rowName = lineArray(0).head
+
+      lineArray
+        .drop(1)
+        .map(_.toInt)
+        .zipWithIndex
+        .foreach { case (weight, i) =>
+          (rowName, nameArray(i)) match {
+            case ('*', '*') =>
+              for {
+                n1 <- excludedSet
+                n2 <- excludedSet
+              } { builder.update((n1, n2), weight) }
+
+            case ('*', colName) =>
+              excludedSet.foreach(n => builder.update((n, colName), weight))
+
+            case (_, '*') =>
+              excludedSet.foreach(n => builder.update((rowName, n), weight))
+
+            case (_, colName) =>
+              builder.update((rowName, colName) , weight)
+          }
+        }
+    }
+
+    val init = builder.filterKeys(t => t._2 >= t._1).toMap
+
+    init.withDefault {
+      case (k1, k2) if k2 < k1 => init(k2, k1)
+    }
+  }
+
+  private def splitBySpaces(str: String) = str.split("\\s+")
+
+  private def getResourceFile(fileName: String): File = {
+    new File(getClass.getClassLoader.getResource(fileName).toURI)
+  }
+}
