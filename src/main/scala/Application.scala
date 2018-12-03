7e9aa342de003e817ce14b9f5aaa118a7c3efa91
Anton Semenov <tochka313@gmail.com>
2018-12-03 15:57:36 +0300

    legacy

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -3,6 +3,7 @@ import ru.bmstu.bioinformatics.Utils._
 import ru.bmstu.bioinformatics.algo.input.SeqPair
 import ru.bmstu.bioinformatics.algo.output.AlignResult
 import ru.bmstu.bioinformatics.algo.util.{DiagSum, DotPlot, Strip}
+import ru.bmstu.bioinformatics.algo_legacy.SmithWatermanRaw
 import ru.bmstu.bioinformatics.database.converted.{Converter, DatabaseOperator}
 import ru.bmstu.bioinformatics.database.initial.{FileReader => OldDbReader}
 import ru.bmstu.bioinformatics.scoring.{SubstringMatchMatrix, WeightMatrix}
@@ -24,21 +25,32 @@ object Application {
 
     val seq = "MVHLTPEEKSAVTALWGKVNVDEVGGEALGRLLVVYPWTQRFFESFGDLSTPDAVMGNPKVKAHGKKVLGAFSDGLAHLDNLKGTFATLSELHCDKLHVDPENFRLLGNVLVCVLAHHFGKEFTPPVQAAYQKVVAGVANALAHKYH"
 
-    val substrings = DotPlot.substrings(seq)
+//    val substrings = DotPlot.substrings(seq)
+    var max: (String, Int) = ("", Int.MinValue)
+    var max2: (String, Int) = ("", Int.MinValue)
 
     DatabaseOperator.read().foreach { case (id, entry) =>
-      val dotplot = DotPlot.apply(ssmm, entry.substrings, substrings)
-      val seqPair = SeqPair(DotPlot.toString(entry.substrings), seq)
+//      val dotplot = DotPlot.apply(ssmm, entry.substrings, substrings)
+//      val seqPair = SeqPair(DotPlot.toString(entry.substrings), seq)
 
-      val diagSum       = DiagSum.fromDotMatrix(dotplot, seqPair.s1.length, seqPair.s2.length, 2)
-      val bestOffsets   = diagSum.pickMax(diagonalFilter)
-      val bestDiags     = bestOffsets.map(seqPair.getDiagonalSeqs)
-      //val bestTrimDiags = bestDiags.map(_.trimmedToMaxLocal(scoreTable))
-      val strips        = Strip.scanlineDiagsFitStrip(stripMaxWidth)(bestOffsets)
-      val stripAligns   = strips.map(_.smithWatermanScore(gapPenalty)(seqPair, weightMatrix))
-      val alignRes      = AlignResult.fromStripAligns(stripAligns)
+//      val diagSum       = DiagSum.fromDotMatrix(dotplot, seqPair.s1.length, seqPair.s2.length, 2)
+//      val bestOffsets   = diagSum.pickMax(diagonalFilter)
+//      val bestDiags     = bestOffsets.map(seqPair.getDiagonalSeqs)
+//      //val bestTrimDiags = bestDiags.map(_.trimmedToMaxLocal(scoreTable))
+//      val strips        = Strip.scanlineDiagsFitStrip(stripMaxWidth)(bestOffsets)
+//      val stripAligns   = strips.map(_.smithWatermanScore(gapPenalty)(seqPair, weightMatrix))
+//      val alignRes      = AlignResult.fromStripAligns(stripAligns)
 
-      println(s"$id -> ${alignRes.score}")
+      val comp = DotPlot.toString(entry.substrings)
+      val score = SmithWatermanRaw.sw(weightMatrix, comp, seq)
+      if (score > max._2) {
+        max = (entry.name, score)
+      } else if (score > max2._2) {
+        max2 = (entry.name, score)
+      }
     }.await()
+
+    println(max)
+    println(max2)
   }
 }
---
+++ src/main/scala/ru/bmstu/bioinformatics/algo_legacy/SmithWatermanRaw.scala
@@ -0,0 +1,30 @@
+package ru.bmstu.bioinformatics.algo_legacy
+
+import ru.bmstu.bioinformatics.scoring.WeightMatrix.KeyMatrix
+
+object SmithWatermanRaw {
+
+  def sw(scoreTable: KeyMatrix, s1: String, s2: String): Int = {
+    val gapPenalty = -1
+
+    var res = 0
+
+    val m = Array.fill(s1.length, s2.length)(0)
+
+    for {
+      i <- 0 until s1.length
+      j <- 0 until s2.length
+    } {
+      val left    = if (j == 0) 0 else m(i)(j - 1)
+      val top     = if (i == 0) 0 else m(i - 1)(j)
+      val topLeft = if (i == 0 || j == 0) 0 else m(i - 1)(j - 1)
+      val score = math.max(math.max(left + gapPenalty, top + gapPenalty), topLeft + scoreTable((s1(i), s2(j))))
+
+      if (i == s1.length - 1 || j == s2.length - 1) {
+        res = math.max(score, res)
+      }
+    }
+
+    res
+  }
+}
