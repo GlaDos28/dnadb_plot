a96f402cc6477475f8d0ac9f5ddbc232506c9e03
Anton Semenov <tochka313@gmail.com>
2018-12-10 01:01:53 +0300

    Revert "algorithm completion and performance adjustments"

--- src/main/scala/ru/bmstu/bioinformatics/algo/util/diagGraph/GraphEdge.scala
+++
@@ -1,5 +0,0 @@
-package ru.bmstu.bioinformatics.algo.util.diagGraph
-
-import ru.bmstu.bioinformatics.algo.input.SeqPair
-
-class GraphEdge(val outInd: Int, val weight: Int, val diagRef: Option[SeqPair] = None)
--- src/main/scala/ru/bmstu/bioinformatics/algo/util/diagGraph/GraphNode.scala
+++
@@ -1,5 +0,0 @@
-package ru.bmstu.bioinformatics.algo.util.diagGraph
-
-import ru.bmstu.bioinformatics.algo.input.SeqPair
-
-class GraphNode(val ind: Int, val seqPairRef: SeqPair, val pos: (Int, Int), var outEdges: List[GraphEdge] = Nil)
