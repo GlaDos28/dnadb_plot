c40dffc6481a255b9ad776a45a64582c9bedfdd4
Eugeny Savelyev <eugenysavelyev@Eugenys-MacBook-Pro.local>
2018-12-10 01:35:42 +0300

    optimized Smith-Waterman and some small optimizations

--- src/main/scala/Application.scala
+++ src/main/scala/Application.scala
@@ -1,6 +1,6 @@
 import ru.bmstu.bioinformatics.Utils._
 import ru.bmstu.bioinformatics.algo.input.SeqPair
-import ru.bmstu.bioinformatics.algo.output.AlignResult
+import ru.bmstu.bioinformatics.algo.util.diagGraph.DiagGraph
 import ru.bmstu.bioinformatics.algo.util.{DiagSum, DotPlot, Strip}
 import ru.bmstu.bioinformatics.algo_legacy.SmithWatermanRaw
 import ru.bmstu.bioinformatics.database.converted.DatabaseOperator
@@ -13,9 +13,9 @@ import scala.concurrent.ExecutionContext.Implicits.global
 //    DatabaseOperator.write(converted)
 object Application {
 
-  val gapPenalty: Int = -2 //штраф за гэп
-  val diagonalFilter: Int = 10 //число отбираемых диагоналей
-  val stripMaxWidth: Int = 7 //максимальная ширина полосы
+  val gapPenalty:     Int = -2 // Штраф за гэп
+  val diagonalFilter: Int = 10 // Число отбираемых диагоналей
+  val cutoffScore:    Int = 28 // Минимальный score диагонали
 
   def main(args: Array[String]): Unit = {
     val weightMatrix = WeightMatrix.readDefault
@@ -37,21 +37,27 @@ object Application {
       val dotplot = DotPlot.apply(ssmm, entry.substrings, substrings)
       val seqPair = SeqPair(entry.sequence, seq)
 
-      val diagSum       = DiagSum.fromDotMatrix(dotplot, seqPair.s1.length, seqPair.s2.length, 2)
-      val bestOffsets   = diagSum.pickMax(diagonalFilter)
-//      val bestDiags     = bestOffsets.map(seqPair.getDiagonalSeqs)
-//      val bestTrimDiags = bestDiags.map(_.trimmedToMaxLocal(scoreTable))
-      val strips        = Strip.scanlineDiagsFitStrip(stripMaxWidth)(bestOffsets)
-      val stripAligns   = strips.map(_.smithWatermanScore(gapPenalty)(seqPair, weightMatrix))
-      val alignRes      = AlignResult.fromStripAligns(stripAligns)
+      val diagSum            = DiagSum.fromDotMatrix(dotplot, seqPair.s1.length, seqPair.s2.length, 2)
+      val bestOffsets        = diagSum.pickMax(diagonalFilter)
+      val bestDiags          = bestOffsets.map(seqPair.getDiagonalSeqs)
+      val bestTrimDiags      = bestDiags.map(_.trimmedToMaxLocal(weightMatrix))
+      var cutDiags           = bestTrimDiags.filter(_.getScore(weightMatrix) >= cutoffScore)
+
+      if (cutDiags.isEmpty) {
+        cutDiags = bestTrimDiags.maxBy(_.getScore(weightMatrix)) :: Nil
+      }
+
+      val graphFilteredDiags = DiagGraph.fromDiags(cutDiags, gapPenalty)(weightMatrix).getUsedDiags
+      val strip              = new Strip(graphFilteredDiags.toList.map(_.diag).sortBy(-_.offset))
+      val alignRes           = strip.smithWatermanScore(gapPenalty)(seqPair, weightMatrix)
 
       if (id % 10000 == 0) {
-        println(id, alignRes.score)
+        println(id, alignRes.score, (System.currentTimeMillis() - timestart).asInstanceOf[Float] / 1000)
       }
 
     }.await()
 
-    println("time", System.currentTimeMillis() - timestart)
+    println("Total time:", System.currentTimeMillis() - timestart)
 
     println(max)
     println(max2)
